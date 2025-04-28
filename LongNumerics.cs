a44197ec96afead8223ee1eb08c7f79ae46213c5
delilit <lexalexagggame@gmail.com>
2025-04-28 20:49:24 +0500

    operators reload

--- LongNumerics.cs
+++ LongNumerics.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO.Pipelines;
 using System.Text;
+using System.Threading.Channels;
 
 namespace Longnumerics{
 public class BigInt
@@ -35,12 +36,89 @@ public class BigInt
         }
         Console.Write("\n");
     }
-    public static BigInt operator +(BigInt a, BigInt b){
-        if (a.isNegative != b.isNegative){
-            
+    public static bool operator == (BigInt a, BigInt b){
+        if (a.isNegative != b.isNegative)
+            return false;
+        if (a.digits.Length != b.digits.Length){
+            return false;
         }
+        for (int i = 0; i<a.digits.Length; i++){
+            if (a.digits[i]!=b.digits[i]){
+                return false;
+            }
+        }
+        return true;
     }
-    
+
+    public static bool operator != (BigInt a, BigInt b){
+        if (a.isNegative != b.isNegative)
+            return true;
+        if (a.digits.Length != b.digits.Length){
+            return true;
+        }
+        for (int i = 0; i<a.digits.Length; i++){
+            if (a.digits[i]!=b.digits[i]){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static bool operator > (BigInt a, BigInt b){
+            bool notequal = a.isNegative != b.isNegative;
+            if (notequal){
+            if (a.isNegative == false & b.isNegative == true){
+                return true;
+            }
+            else return false;
+            }
+            if (a.isNegative == false){
+                if (a.digits.Length > b.digits.Length) return true;
+                if (a.digits.Length < b.digits.Length) return false;
+                for (int i = 0; i< a.digits.Length; i++){
+                    if (a.digits[i] < b.digits[i]) return false;
+                }
+            }
+            else{
+                if (a.digits.Length > b.digits.Length) return false;
+                if (a.digits.Length < b.digits.Length) return true;
+                for (int i = 0; i< a.digits.Length; i++){
+                    if (a.digits[i] < b.digits[i]) return true;
+                }
+        }
+        return false;
+    }
+
+    public static bool operator < (BigInt a, BigInt b){
+            bool notequal = a.isNegative != b.isNegative;
+            if (notequal){
+            if (a.isNegative == false & b.isNegative == true){
+                return false;
+            }
+            else return true;
+            }
+            if (a.isNegative == false){
+                if (a.digits.Length > b.digits.Length) return false;
+                if (a.digits.Length < b.digits.Length) return true;
+                for (int i = 0; i< a.digits.Length; i++){
+                    if (a.digits[i] < b.digits[i]) return true;
+                }
+            }
+            else{
+                if (a.digits.Length > b.digits.Length) return true;
+                if (a.digits.Length < b.digits.Length) return false;
+                for (int i = 0; i< a.digits.Length; i++){
+                    if (a.digits[i] < b.digits[i]) return false;
+                }
+        }
+        return false;
+    }
+
+    public static BigInt operator + (BigInt a, BigInt b){                
+        
+        return new BigInt("1234");
+    }
+
 
 }
 }
--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@
 class Program{
     static void Main(){
         BigInt big = new BigInt("12344123");
+        BigInt big2 = new BigInt("-12344123");
         big.print();
+        Console.WriteLine(big == big2);
     }
 }
